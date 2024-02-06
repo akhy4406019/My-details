8aaf593b5e20eccf38b4f07f84636ba38a140b45
akhy4406019 <149380498+akhy4406019@users.noreply.github.com>
2024-02-06 23:32:05 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,6 @@
+const my_details= {my_name:"Akhy",my_age:"17",my_dob:"06/06/2006",father:"Shepon Howlader",mother:"Najnin" };
+document.getElementById('name').innerHTML = "My name is" + " " + my_details.my_name;
+document.getElementById('age').innerHTML = "My age is" + " " + my_details.my_age;
+document.getElementById('dob').innerHTML = "My date of birth is" + " " + my_details.my_dob;
+document.getElementById('father').innerHTML = "My father is" + " " + my_details.father;
+document.getElementById('mother').innerHTML = "My mother is" + " " + my_details.mother;
